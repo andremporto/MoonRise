24b125830cc53f48a7392c5c9774d4ce83c6a601
André Porto <andreporto@me.com>
2023-07-06 08:06:26 -0300

    Initial Commit

---
+++ MoonRise/MoonRiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonRiseApp.swift
+//  MoonRise
+//
+//  Created by André Porto on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonRiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
